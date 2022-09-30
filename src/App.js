2fe5fe5069c436389ee4b95cd6ff1ea206d0e4ea
GUchechi <godswilluchendu7@gmail.com>
2022-09-30 14:52:30 +0100

    Mobile toggle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [hiddenMenu, setHiddenMenu] = useState(true);
+
   return (
-    <div className="App">
-    
+    <div className="hero">
+      <nav>
+        <div className={(hiddenMenu ? "hidden" : "") + " menu"}>
+          <a href="#/">Home</a>
+          <a href="#/">Projects</a>
+          <a href="#/">Contact</a>
+        </div>
+        <span onClick={() => setHiddenMenu(!hiddenMenu)}>
+          {hiddenMenu ? "|||" : "X"}
+        </span>
+      </nav>
     </div>
-  );
+  )
 }
 
 export default App;
